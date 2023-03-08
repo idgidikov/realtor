681b504bd597411e10edce029dc5ddd171beb4d9
idgidikov <idgidikov@abv.bg>
2023-03-08 15:59:28 +0200

    feat : add map to the listing page

--- realtor-clone-react/src/pages/Listing.jsx
+++ realtor-clone-react/src/pages/Listing.jsx
@@ -13,40 +13,43 @@ import SwiperCore, {
   Pagination,
 } from "swiper";
 import "swiper/css/bundle";
-import {FaShare,FaMapMarkerAlt, FaBed,
-    FaBath,
-    FaParking,
-    FaChair,} from 'react-icons/fa'
+import {
+  FaShare,
+  FaMapMarkerAlt,
+  FaBed,
+  FaBath,
+  FaParking,
+  FaChair,
+} from "react-icons/fa";
 import { getAuth } from "firebase/auth";
 import Contact from "../components/Contact";
+import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
 
 export default function Listing() {
-    const params = useParams();
-    const auth = getAuth();
-    const [listing,setListing] = useState(null);
-    const [loading,setLoading] = useState(true);
-    const [shareLinkCopied, setShareLinkCopied] = useState(false);
-    const [contactLandlord, setContactLandlord] = useState(false);
-    SwiperCore.use([Autoplay, Navigation, Pagination]);
-
-
-    useEffect(() => {
-        async function fetchListing() {
-          const docRef = doc(db, "listings", params.listingId);
-          const docSnap = await getDoc(docRef);
-          if (docSnap.exists()) {
-            setListing(docSnap.data());
-            setLoading(false);
-          }
-        }
-        fetchListing();
-      }, [params.listingId]);
-      if (loading) {
-        return <Spinner />;
+  const auth = getAuth();
+  const params = useParams();
+  const [listing, setListing] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [shareLinkCopied, setShareLinkCopied] = useState(false);
+  const [contactLandlord, setContactLandlord] = useState(false);
+  SwiperCore.use([Autoplay, Navigation, Pagination]);
+  useEffect(() => {
+    async function fetchListing() {
+      const docRef = doc(db, "listings", params.listingId);
+      const docSnap = await getDoc(docRef);
+      if (docSnap.exists()) {
+        setListing(docSnap.data());
+        setLoading(false);
       }
+    }
+    fetchListing();
+  }, [params.listingId]);
+  if (loading) {
+    return <Spinner />;
+  }
   return (
     <main>
-          <Swiper
+      <Swiper
         slidesPerView={1}
         navigation
         pagination={{ type: "progressbar" }}
@@ -66,25 +69,27 @@ export default function Listing() {
           </SwiperSlide>
         ))}
       </Swiper>
-      <div 
-      className = "fixed top-[13%] right-[3%] z-10 bg-white cursor-pointer border-2 border-gray-400 rounded-full w-12 h-12 flex justify-center items-center"
-      onClick={()=>{
-        navigator.clipboard.writeText(window.location.href);
-        setShareLinkCopied(true);
-        setTimeout(()=>{
+      <div
+        className="fixed top-[13%] right-[3%] z-10 bg-white cursor-pointer border-2 border-gray-400 rounded-full w-12 h-12 flex justify-center items-center"
+        onClick={() => {
+          navigator.clipboard.writeText(window.location.href);
+          setShareLinkCopied(true);
+          setTimeout(() => {
             setShareLinkCopied(false);
-        },2000)
-      }}>
-              <FaShare className="text-lg text-slate-500"/>
+          }, 2000);
+        }}
+      >
+        <FaShare className="text-lg text-slate-500" />
       </div>
-      {shareLinkCopied &&(
-        <p className = "fixed top-[20%] right-[5%] font-semibold border-2 border-gray-400 rounded-md bg-white z-10 p-2">
-            Link Copied
+      {shareLinkCopied && (
+        <p className="fixed top-[23%] right-[5%] font-semibold border-2 border-gray-400 rounded-md bg-white z-10 p-2">
+          Link Copied
         </p>
       )}
-      <div className = "m-4 flex flex-col md:flex-row max-w-6xl lg:mx-auto p-4 rounded  shadow-lg bg-white lg:space-x-5">
-        <div className="w-full h-[200px] lg-[400px]">
-            <p className="text-2xl font-bold mb-3 text-blue-900">
+
+      <div className="m-4 flex flex-col md:flex-row max-w-6xl lg:mx-auto p-4 rounded-lg shadow-lg bg-white lg:space-x-5">
+        <div className=" w-full ">
+          <p className="text-2xl font-bold mb-3 text-blue-900">
             {listing.name} - ${" "}
             {listing.offer
               ? listing.discountedPrice
@@ -94,8 +99,8 @@ export default function Listing() {
                   .toString()
                   .replace(/\B(?=(\d{3})+(?!\d))/g, ",")}
             {listing.type === "rent" ? " / month" : ""}
-            </p>
-            <p className="flex items-center mt-6 mb-3 font-semibold">
+          </p>
+          <p className="flex items-center mt-6 mb-3 font-semibold">
             <FaMapMarkerAlt className="text-green-700 mr-1" />
             {listing.address}
           </p>
@@ -131,7 +136,7 @@ export default function Listing() {
               {listing.furnished ? "Furnished" : "Not furnished"}
             </li>
           </ul>
-          {listing?.userRef !== auth.currentUser?.uid && !contactLandlord && (
+          {listing.userRef !== auth.currentUser?.uid && !contactLandlord && (
             <div className="mt-6">
               <button
                 onClick={() => setContactLandlord(true)}
@@ -145,12 +150,27 @@ export default function Listing() {
             <Contact userRef={listing.userRef} listing={listing} />
           )}
         </div>
-        <div className="bg-blue-300 w-full h-[200px] lg-[400px] z-10 overflow-x-hidden">
-
+        <div className="w-full h-[200px] md:h-[400px] z-10 overflow-x-hidden mt-6 md:mt-0 md:ml-2">
+          <MapContainer
+            center={[listing.geolocation.lat, listing.geolocation.lng]}
+            zoom={13}
+            scrollWheelZoom={false}
+            style={{ height: "100%", width: "100%" }}
+          >
+            <TileLayer
+              attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            />
+            <Marker
+              position={[listing.geolocation.lat, listing.geolocation.lng]}
+            >
+              <Popup>
+                {listing.address}
+              </Popup>
+            </Marker>
+          </MapContainer>
         </div>
       </div>
-            
     </main>
-  
-  )
+  );
 }
