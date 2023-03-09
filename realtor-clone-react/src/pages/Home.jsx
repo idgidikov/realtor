07b4398e52f02cfcc47c8cdee5b4a36a065ac85a
idgidikov <idgidikov@abv.bg>
2023-03-09 16:13:13 +0200

    feat : add slider component

--- realtor-clone-react/src/pages/Home.jsx
+++ realtor-clone-react/src/pages/Home.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
-
+import Slider from '../components/Slider';
 export default function Home() {
   return (
     <div>
       <h1>Home</h1>
-
+      <Slider/>
     </div>
   )
 }
