38ed5c4100cc5087e5c4b8b3a21902dc33612800
idgidikov <idgidikov@abv.bg>
2023-03-29 17:22:36 +0300

    feat : complate offers page

--- realtor-clone-react/src/pages/Home.jsx
+++ realtor-clone-react/src/pages/Home.jsx
@@ -1,14 +1,12 @@
 import {
   collection,
-  getDoc,
   getDocs,
   limit,
   orderBy,
   query,
   where,
 } from "firebase/firestore";
-import { useEffect } from "react";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import ListingItem from "../components/ListingItem";
 import Slider from "../components/Slider";
