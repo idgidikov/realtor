60483366610eed302b8cfbc91ffe36003c398905
idgidikov <idgidikov@abv.bg>
2023-03-29 19:18:52 +0300

    fix : folder name because deployment

---
+++ realtor_clone_react/src/components/PrivateRoute.jsx
@@ -0,0 +1,10 @@
+import { Outlet, Navigate } from "react-router-dom";
+import { UseAuthStatus } from "../hooks/UseAuthStatus";
+import Spinner from "./Spinner";
+export default function PrivateRoute() {
+  const { loggedIn, checkingStatus } = UseAuthStatus();
+  if (checkingStatus) {
+    return <Spinner />;
+  }
+  return loggedIn ? <Outlet /> : <Navigate to="/sign-in" />;
+}
---
+++ realtor_clone_react/src/pages/SignIn.jsx
@@ -0,0 +1,93 @@
+import React from 'react'
+import { useState } from 'react';
+import {AiFillEyeInvisible , AiFillEye} from "react-icons/ai"
+import { Link ,useNavigate} from 'react-router-dom';
+import OAuth from '../components/OAuth';
+import { signInWithEmailAndPassword, getAuth } from 'firebase/auth';
+import { toast } from "react-toastify";
+
+
+
+
+function SignIn() {
+  const [showPassword, setShowPassword] = useState(false);
+  const [formData, setFormData] = useState({
+    email : "",
+    password: ""
+  })
+  const navigate = useNavigate();
+
+  const {email,password} = formData;
+
+  function onChange(e) {
+    setFormData((prevState)=>({
+      ...prevState,
+      [e.target.id] : e.target.value
+    }));
+
+  }
+  async function onSubmit(e) {
+    e.preventDefault();
+    try{
+      const auth = getAuth()
+      const userCredentials = await signInWithEmailAndPassword(auth,email,password);
+      if(userCredentials.user){
+        navigate('/')
+      }
+    }catch(error){
+      toast.error("Bad user credentials")
+    }
+  }
+  return (
+    <section>
+      <h1 className='text-3xl text-center mt-6 font-bold'>Sign in</h1>
+      <div className='flex justify-center flex-wrap items-center px-6 py-12 max-w-6xl mx-auto'>
+        <div className='md:w-[67%] lg:w-[50%] mb-12 md:mb-6'>
+          <img className='w-full rounded-2xl'
+          src="https://images.unsplash.com/flagged/photo-1564767609342-620cb19b2357?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1073&q=80" alt="key" />
+        </div>
+        <div className='w-full md:w-[67%] lg:w-[40%] lg:ml-20'>
+          <form onSubmit={onSubmit} >
+            <input placeholder='Email address'
+            className='mb-6 w-full px-4 py-2 text-xl text-gray-700 bg-white border-gray-300
+            rounded transition ease-in-out' 
+            type="email" id='email' value={email} onChange = {onChange}/>
+            <div className='relative mb-6'>
+            <input placeholder='password'
+            className='w-full px-4 py-2 text-xl text-gray-700 bg-white border-gray-300
+            rounded transition ease-in-out' 
+            type={showPassword ? 'text' : 'password'} id='password' value={password} onChange = {onChange}/>
+            {showPassword ? 
+              <AiFillEyeInvisible className='absolute right-3 top-3 text-xl cursor-pointer'
+                onClick={()=> setShowPassword((prev)=> !prev)}
+              /> : <AiFillEye className='absolute right-3 top-3 text-xl cursor-pointer'
+              onClick={()=> setShowPassword((prev)=> !prev)}
+              />
+            }
+            </div>
+           <div className='flex justify-between whitespace-nowrap text-sm sm:text-lg'>
+           <p className='-mb-60'>Don't have a account?
+            <Link className='text-red-600 hover:red-700 transition duration-200 ease-in-out ml-1' to="/sign-up">Register</Link>
+            </p>
+            <p>
+            <Link className='text-blue-600 hover:blue-700 transition duration-200 ease-in-out ml-1'
+             to="/forgot-pass">Forgot password?</Link>
+            </p>
+           </div>
+           <button className='w-full bg-blue-600 text-white px-7 py-3 
+          text-sm font-medium uppercase rounded shadow-md hover:bg-blue-700 transition duration-150 ease-in-out hover:shadow-lg active:bg-blue-800' type='submit'>Sign in</button>
+          <div className='my-4 before:border-t flex before:flex-1 items-center before:border-gray-300
+          after:border-t  after:flex-1 after:border-gray-300'>
+            <p className='text-center font-semibold mx-4'>OR</p>
+          </div>
+          <OAuth/>
+          </form>
+          
+        </div>
+
+      </div>
+    </section>
+  )
+}
+
+export default SignIn
